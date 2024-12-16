3d9f53fc849cf4bc69b98cee9e22e064137c11ea
JOSE MIGUEL OROZCO MARTINEZ <josemiguelorozcomartinez687@gmail.com>
2024-12-16 12:09:01 -0500

    git push

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Home from "./pages/Home";
+
+const App = () => {
+  return (
+    <div className="w-screen h-screen bg-slate-400">
+      <Home />
+    </div>
+  )
+};
+
+export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+            <h1 className="text-4xl font-bold text-blue-500">Bienvenido a mi proyecto</h1>
+            <p className="text-gray-700 mt-4">Este es un proyecto básico con React y TypeScript.</p>
+        </div>
+    );
+};
+
+export default Home;
